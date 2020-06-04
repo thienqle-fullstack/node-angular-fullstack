30f0c0e666a6dc74a12e97b220d9047c1f612f35
T L <thienlequangvn@gmail.com>
2020-06-04 19:22:49 -0400

    update IP

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', function (req, res) {
 
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
-// var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+//var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function () {
     console.log("Server running");
